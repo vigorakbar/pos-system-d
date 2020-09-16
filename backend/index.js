f677a209337b3cce108cc9ae23d3179a984d9c13
Vigor Akbar <vigorakbar@gmail.com>
2020-09-16 23:48:42 +0700

    setting cors in express

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const config = require("config");
 const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
 const usersRoute = require("./routes/user.route");
 const authRoute = require("./routes/auth.route")
-const express = require("express");
 const app = express();
 
 
@@ -22,12 +23,13 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB...", err));
 
-
+const clientPort = process.env.CLIENT_PORT || 3000;
+app.use(cors({ origin: `http://localhost:${clientPort}` }))
 app.use(express.json());
 
 // routes
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
